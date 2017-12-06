ffc098a71dcb58d87528ff269149e47c65aef024
duanchaozhong <857999639@qq.com>
2017-12-06 17:34:22 +0800

    a

--- app/src/main/java/com/example/duan/chao/DCZ_application/MyApplication.java
+++ app/src/main/java/com/example/duan/chao/DCZ_application/MyApplication.java
@@ -115,8 +115,8 @@ public class MyApplication extends Application{
         language=sf.getString("language","");
         offset=sf.getLong("offset",0);
         cookie=sf.getString("cookie","");
-        uri=sf.getString("uri","http://110.79.11.5/user-safe-api/");
-        ur=sf.getString("ur","http://test-makeys.qeveworld.com/#/register");
+        uri=sf.getString("uri","http://api.qeveworld.com/user-safe-api/");
+        ur=sf.getString("ur","http://makeys.qeveworld.com/#/register");
         PIN_LENGTH=sf.getInt("PIN_LENGTH",8);
         DEFAULT_INTERVAL=sf.getInt("DEFAULT_INTERVAL",30);
         Log.i("dcz_first",first+"");
